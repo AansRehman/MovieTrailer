59cc97b8e6c0ea73220f937eaa28073a201ab265
AansRehman <106799284+AansRehman@users.noreply.github.com>
2023-06-22 22:32:49 +0500

    backed completed in spring boot

---
+++ src/main/java/com/example/movietrailerapp/MovieTrailerAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.movietrailerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MovieTrailerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTrailerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/movietrailerapp/MovieTrailerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movietrailerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTrailerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
